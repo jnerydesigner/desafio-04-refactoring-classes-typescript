5bcd9040b72f0397e77280a5106d0abe7152551e
jnerydesigner <jander.webmaster@gmail.com>
2021-03-29 17:57:28 -0400

    Conclusion React Challenge Rocketseat

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,23 @@ import {
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes
 } from 'react';
+
+import { IconBaseProps } from 'react-icons';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+export function Input({ name, icon: Icon, ...rest }: IInputProps) {
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -49,5 +58,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
